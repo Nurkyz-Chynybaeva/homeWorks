0e6b1c7a0104efed158160ce0cb69573d74e0b01
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-10-08 12:24:01 +0600

    home work 6.10.21

--- src/App/App.kt
+++ src/App/App.kt
@@ -1,13 +1,34 @@
-package App
 
-class App {
-
-}
 
 fun main(){
-    val x1 = readLine()!!.toInt()
-    val x2 = readLine()!!.toInt()
-    val res = x1 / x2
-    println("res= ${res}")
+
+
+
+    val x1 = 55
+    val x2 = 55
+    val x = x1 == x2
+
+    val a2= when{
+        x -> "true"
+        else -> "false"
+    }
+    println(a2)
+
+//    val a = readLine()!!.toInt()
+//    val x1 = 55
+//    val x2 = 55
+//    val x = x1 == x2
+//    val a3 = a == x
+//
+//   val a2= when{
+//       a3 -> "true"   // a3
+//        else -> "false"
+//    }
+//    println(a2)                             Kotlin error: Operator '==' cannot be applied to 'Int' and 'Boolean'
+
+
+
+
+
 
 }
